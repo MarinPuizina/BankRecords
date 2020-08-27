ffd0e7e164053660d263b2285badc0eb0271d4ef
MarinPuizina <marinpuizina@hotmail.com>
2020-08-27 12:16:08 +0200

    Initial commit

---
+++ src/main/java/com/marin/bankrecords/BankRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.marin.bankrecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankRecordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marin/bankrecords/BankRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marin.bankrecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankRecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
