dacaaad045c81bbc564beb8de47339107dd88232
MarinPuizina <marinpuizina@hotmail.com>
2020-08-27 12:28:13 +0200

    Add status check for bank records system

--- src/main/java/com/marin/bankrecords/ui/controller/BankRecordController.java
+++ src/main/java/com/marin/bankrecords/ui/controller/BankRecordController.java
@@ -1,4 +1,16 @@
 package com.marin.bankrecords.ui.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bank/records")
 public class BankRecordController {
+
+    @GetMapping("/status/check")
+    public String statusCheck() {
+        return "The Bank Records system is operational.";
+    }
+
 }
