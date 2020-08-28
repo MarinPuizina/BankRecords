a91eb1b05421669332f51785c22fd16b92957ffb
MarinPuizina <marinpuizina@hotmail.com>
2020-08-28 17:16:21 +0200

    Add column fields for entity class

--- src/main/java/com/marin/bankrecords/data/Clients.java
+++ src/main/java/com/marin/bankrecords/data/Clients.java
@@ -1,4 +1,22 @@
 package com.marin.bankrecords.data;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+
+@Entity
 public class Clients {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String group;
+
 }
