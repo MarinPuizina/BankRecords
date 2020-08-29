b34f6102d19a959320518d9fe7a16cf58e6f98aa
MarinPuizina <marinpuizina@hotmail.com>
2020-08-29 15:40:37 +0200

    Add testCreateClient

--- src/test/java/com/marin/bankrecords/data/repository/ClientsRepositoryTest.java
+++ src/test/java/com/marin/bankrecords/data/repository/ClientsRepositoryTest.java
@@ -1,12 +1,26 @@
 package com.marin.bankrecords.data.repository;
 
+import com.marin.bankrecords.data.entitiy.Clients;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class ClientsRepositoryTest {
 
     @Autowired
     ClientsRepository clientsRepository;
+
+    @Test
+    public void testCreateClient() {
+
+        Clients client = clientsRepository.save(new Clients("Test Name", "Rich"));
+
+        assertNotNull(client);
+        assertTrue(client.getId() > 0);
+
+    }
 
 }
