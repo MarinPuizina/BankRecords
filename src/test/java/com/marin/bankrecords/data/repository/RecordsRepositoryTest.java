51a0edc22e3a7c8303d579ccc99dd647c106d012
MarinPuizina <marinpuizina@hotmail.com>
2020-08-30 13:59:18 +0200

    Add RecordsRepositoryTest.java

---
+++ src/test/java/com/marin/bankrecords/data/repository/RecordsRepositoryTest.java
@@ -0,0 +1,10 @@
+package com.marin.bankrecords.data.repository;
+
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("<= Records Repository Test =>")
+class RecordsRepositoryTest {
+
+}
