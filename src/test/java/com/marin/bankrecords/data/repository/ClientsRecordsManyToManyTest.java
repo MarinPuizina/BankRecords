89f690d0c35eca80a82b38c2eea2eb37209497d4
MarinPuizina <marinpuizina@hotmail.com>
2020-08-30 14:29:58 +0200

    Add ClientsRecordsManyToManyTest.java

---
+++ src/test/java/com/marin/bankrecords/data/repository/ClientsRecordsManyToManyTest.java
@@ -0,0 +1,17 @@
+package com.marin.bankrecords.data.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+
+@DisplayName("<= Clients and Records Many To Many Test")
+public class ClientsRecordsManyToManyTest {
+
+    
+
+}
