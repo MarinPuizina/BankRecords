54fa8b06405413211fb06615f109da0e5578d207
MarinPuizina <marinpuizina@hotmail.com>
2020-08-30 14:03:05 +0200

    Add recordsRepositoryShouldSaveRecord test

--- src/test/java/com/marin/bankrecords/data/repository/RecordsRepositoryTest.java
+++ src/test/java/com/marin/bankrecords/data/repository/RecordsRepositoryTest.java
@@ -1,10 +1,25 @@
 package com.marin.bankrecords.data.repository;
 
+import com.marin.bankrecords.data.entitiy.Records;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DisplayName("<= Records Repository Test =>")
 class RecordsRepositoryTest {
+
+    @Autowired
+    RecordsRepository recordsRepository;
+
+    @Test
+    void recordsRepositoryShouldSaveRecord() {
+
+        Records record = recordsRepository.save(new Records("Invoice"));
+
+        assertNotNull(record);
+
+    }
 
 }
