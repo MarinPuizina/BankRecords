e9e21ce25a6a373d8b2dec777878e09fc7b70a24
MarinPuizina <marinpuizina@hotmail.com>
2020-08-30 13:47:43 +0200

    Edit Adding @Repository to repository interfaces

--- src/main/java/com/marin/bankrecords/data/repository/ClientsRepository.java
+++ src/main/java/com/marin/bankrecords/data/repository/ClientsRepository.java
@@ -2,6 +2,8 @@ package com.marin.bankrecords.data.repository;
 
 import com.marin.bankrecords.data.entitiy.Clients;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientsRepository extends CrudRepository<Clients, Integer> {
 }
--- src/main/java/com/marin/bankrecords/data/repository/RecordsRepository.java
+++ src/main/java/com/marin/bankrecords/data/repository/RecordsRepository.java
@@ -2,6 +2,8 @@ package com.marin.bankrecords.data.repository;
 
 import com.marin.bankrecords.data.entitiy.Records;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RecordsRepository extends CrudRepository<Records, Integer> {
 }
