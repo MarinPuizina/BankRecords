b99fa4f7ff42c165e6014ce3fcc9012ae343d62e
MarinPuizina <marinpuizina@hotmail.com>
2020-08-31 14:53:51 +0200

    Edit after removing cascade type, added save for records

--- src/main/java/com/marin/bankrecords/BankRecordsApplication.java
+++ src/main/java/com/marin/bankrecords/BankRecordsApplication.java
@@ -3,6 +3,7 @@ package com.marin.bankrecords;
 import com.marin.bankrecords.data.entitiy.Clients;
 import com.marin.bankrecords.data.entitiy.Records;
 import com.marin.bankrecords.data.repository.ClientsRepository;
+import com.marin.bankrecords.data.repository.RecordsRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -13,12 +14,14 @@ public class BankRecordsApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(BankRecordsApplication.class, args);
 
+		RecordsRepository recordsRepository = context.getBean(RecordsRepository.class);
 		ClientsRepository clientsRepository = context.getBean(ClientsRepository.class);
 
 		Records record = new Records("Invoice");
 		Clients client = new Clients("Test Name", "Rich");
 		client.addRecord(record);
 
+		recordsRepository.save(record);
 		clientsRepository.save(client);
 
 	}
