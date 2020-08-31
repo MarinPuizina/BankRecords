cf8edaf35dd4d83bd35531a0086cc7dab18d6a49
MarinPuizina <marinpuizina@hotmail.com>
2020-08-31 13:41:32 +0200

    Add code sample to simulate ManyToMany and cascade.All

--- src/main/java/com/marin/bankrecords/BankRecordsApplication.java
+++ src/main/java/com/marin/bankrecords/BankRecordsApplication.java
@@ -1,13 +1,26 @@
 package com.marin.bankrecords;
 
+import com.marin.bankrecords.data.entitiy.Clients;
+import com.marin.bankrecords.data.entitiy.Records;
+import com.marin.bankrecords.data.repository.ClientsRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BankRecordsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BankRecordsApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(BankRecordsApplication.class, args);
+
+		ClientsRepository clientsRepository = context.getBean(ClientsRepository.class);
+
+		Records record = new Records("Invoice");
+		Clients client = new Clients("Test Name", "Rich");
+		client.addRecord(record);
+
+		clientsRepository.save(client);
+
 	}
 
 }
