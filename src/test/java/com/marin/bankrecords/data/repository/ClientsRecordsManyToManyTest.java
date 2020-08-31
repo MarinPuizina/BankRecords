678833d9cea5ecfc403713f3e688e72af571719d
MarinPuizina <marinpuizina@hotmail.com>
2020-08-31 13:40:04 +0200

    Add manyToManyShouldSaveData test method

--- src/test/java/com/marin/bankrecords/data/repository/ClientsRecordsManyToManyTest.java
+++ src/test/java/com/marin/bankrecords/data/repository/ClientsRecordsManyToManyTest.java
@@ -1,5 +1,7 @@
 package com.marin.bankrecords.data.repository;
 
+import com.marin.bankrecords.data.entitiy.Clients;
+import com.marin.bankrecords.data.entitiy.Records;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,21 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("<= Clients and Records Many To Many Test")
 public class ClientsRecordsManyToManyTest {
 
-    
+    @Autowired
+    ClientsRepository clientsRepository;
+
+    @Autowired
+    RecordsRepository recordsRepository;
+
+    @Test
+    void manyToManyShouldSaveData() {
+
+        Records record = new Records("Invoice");
+        Clients client = new Clients("Test Name", "Rich");
+        client.addRecord(record);
+
+        Clients clientTest = clientsRepository.save(client);
+
+    }
 
 }
