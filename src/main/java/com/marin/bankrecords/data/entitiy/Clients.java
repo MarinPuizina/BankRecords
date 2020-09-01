9c13d2961fa25b8cc8ab7941d9b49c423fc7bbae
MarinPuizina <marinpuizina@hotmail.com>
2020-09-01 11:43:41 +0200

    Edit changed int to Long for @Id since that's the best practice when using Hibernate

--- src/main/java/com/marin/bankrecords/data/entitiy/Clients.java
+++ src/main/java/com/marin/bankrecords/data/entitiy/Clients.java
@@ -14,7 +14,7 @@ public class Clients {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     private String name;
     private String group;
