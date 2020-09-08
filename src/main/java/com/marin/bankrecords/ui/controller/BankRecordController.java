e6bb276ea566664393081392d6d7fc7af3117013
MarinPuizina <marinpuizina@hotmail.com>
2020-09-08 15:04:25 +0200

    Add createBankRecordData POST method

--- src/main/java/com/marin/bankrecords/ui/controller/BankRecordController.java
+++ src/main/java/com/marin/bankrecords/ui/controller/BankRecordController.java
@@ -1,6 +1,8 @@
 package com.marin.bankrecords.ui.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +13,14 @@ public class BankRecordController {
     @GetMapping("/status/check")
     public String statusCheck() {
         return "The Bank Records system is operational.";
+    }
+
+    @PostMapping(
+            consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}
+            )
+    public String createBankRecordData() {
+        return "";
     }
 
 }
